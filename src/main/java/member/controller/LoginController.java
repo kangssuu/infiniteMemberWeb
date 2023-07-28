e85dc68785d0d2fff3eddf9983cda2cdb888930c
ssuye <tndp9922@gmail.com>
2023-07-28 18:13:08 +0900

    [ADD] 23/07/28 회원 로그인, 로그아웃 기능 추가
    회원 로그인, 로그아웃 컨트롤러 추가

---
+++ src/main/java/member/controller/LoginController.java
@@ -0,0 +1,69 @@
+package member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import member.model.service.MemberService;
+import member.model.vo.Member;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/member/login.do")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String memberId = request.getParameter("member-id");
+		String memberPw = request.getParameter("member-pw");
+		Member member = new Member(memberId, memberPw);
+		
+		// SELECT * FROM MEMBER_TBL WHERE MEMBER_ID = ? AND MEMBER_PW = ?
+		MemberService service = new MemberService();
+		Member mOne = service.selectCheckLogin(member);
+//		Member mOne = service.selectCheckLogin(memberId, memberPw);  // 처리해야 될 값이나 경우에 따라서 나눠서 넘길 수 있음
+		if (mOne != null) {
+			HttpSession session = request.getSession();
+			session.setAttribute("memberId", mOne.getMemberId());
+			session.setAttribute("memberName", mOne.getMemberName());
+			// 로그인 성공
+			request.setAttribute("msg", "로그인 성공!");
+			request.setAttribute("url", "/index.jsp");
+			RequestDispatcher view = request.getRequestDispatcher("/member/serviceSuccess.jsp");
+			view.forward(request, response);  // 누락주의
+		}
+		else {
+			// 로그인 실패
+			request.setAttribute("msg", "로그인 되지 않았습니다.");
+			RequestDispatcher view = request.getRequestDispatcher("/member/serviceFailed.jsp");
+			view.forward(request, response);  // 누락주의
+		}
+	}
+
+}
